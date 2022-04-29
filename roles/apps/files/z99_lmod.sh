550662e55be614240fdd8f4e726d4302b233dfea
Dane Kennedy <kennedy.dane@gmail.com>
2022-04-29 17:12:46 +0200

    Only add to path if not there already

--- roles/apps/files/z99_lmod.sh
+++ roles/apps/files/z99_lmod.sh
@@ -1,1 +1,8 @@
-export MODULEPATH=/software/modules/common:/software/modules/bio:/software/modules/astro:${MODULEPATH}
+for mpath in /software/modules/common /software/modules/bio /software/modules/astro
+do
+  if [[ "${mpath}" != *"${MODULEPATH}"* ]]; then
+     MODULEPATH="${mpath}:${MODULEPATH}"
+  fi
+done
+
+export MODULEPATH
